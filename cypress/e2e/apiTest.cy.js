511b19355344fa7da39c46220af91f61bf6b9dee
gonzalorp <145234258+gonzalorp@users.noreply.github.com>
2023-09-17 21:39:11 -0300

    Update apiTest.cy.js
    Add comments

--- cypress/e2e/apiTest.cy.js
+++ cypress/e2e/apiTest.cy.js
@@ -1,20 +1,31 @@
+// Describe block to group together related tests 
 describe('Test Public APIs', () => {
 
-    it('gets auth APIs', () => {
-  
-      cy.request('https://api.publicapis.org/entries')
-        .its('body')
-        .then(data => {
-  
-          const authApis = data.entries.filter(entry => {
-            return entry.Category === 'Authentication & Authorization'; 
-          });
-  
-          expect(authApis.length).to.eq(7);
-  
-          cy.log(authApis);
+  // Individual test case  
+  it('gets auth APIs', () => {
+
+    // Make HTTP request to API endpoint
+    cy.request('https://api.publicapis.org/entries')
+
+      // Get just the response body
+      .its('body')
+      
+      // Parse response body JSON
+      .then(data => {
+
+        // Filter entries for Auth category
+        const authApis = data.entries.filter(entry => {
+          return entry.Category === 'Authentication & Authorization'; 
         });
-  
-    });
-  
+
+        // Assertion to check number of auth APIs
+        // Fails if not equal to 7
+        expect(authApis.length).to.eq(7);
+
+        // Log auth APIs to console for debugging
+        cy.log(authApis);
+      });
+
   });
+
+});
